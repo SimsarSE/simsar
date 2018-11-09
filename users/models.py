42b61145b397c226864033c5cfb2f12cb751b9c0
Osman TIKNA <osman@tikna.com>
2018-11-09 22:31:06 +0300

    initial commit

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    phone = PhoneNumberField(unique=True, blank=False)
+
+    def __str__(self):
+        return self.email
