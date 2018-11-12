5fd30cd20ba9a5df54331198c832e2f53e151706
hayrettine <hayrettine@anadolu.edu.tr>
2018-11-12 11:49:55 +0300

    add foreign key to product

--- product/views.py
+++ product/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, redirect, get_object_or_404
+
+from users.models import CustomUser
 from .models import Product
 from .forms import ProductForm
+
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product/product_list.html', {'products': products})
 
+
 def product_new(request):
+    current_user = request.user
     if request.method == "POST":
         form = ProductForm(request.POST)
         if form.is_valid():
             product = form.save(commit=False)
+            product.seller = current_user
             product.save()
             return redirect('product_detail', pk=product.pk)
     else:
         form = ProductForm()
     return render(request, 'product/product_edit.html', {'form': form})
+
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
