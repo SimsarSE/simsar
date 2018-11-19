6dffac58af735ee40b24bf8bd3797b2b058630cd
alperensoydan <alperensoydan@anadolu.edu.tr>
2018-11-19 12:49:28 +0300

    create edit view for product and authentication

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('list', views.product_list, name='product_list'),
     path('new', views.product_new, name='product_new'),
     path('<int:pk>', views.product_detail, name='product_detail'),
+    path('<int:pk>/edit', views.product_edit, name='product_edit'),
     path('product/list', views.product_list, name='home'),
 ]
--- product/views.py
+++ product/views.py
@@ -13,18 +13,37 @@ def product_list(request):
 
 def product_new(request):
     current_user = request.user
-    if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.seller = current_user
-            product.save()
-            return redirect('product_detail', pk=product.pk)
+    if current_user:
+        if request.method == "POST":
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                product = form.save(commit=False)
+                product.seller = current_user
+                product.save()
+                return redirect('product_detail', pk=product.pk)
+        else:
+            form = ProductForm()
+        return render(request, 'product/product_edit.html', {'form': form})
     else:
-        form = ProductForm()
-    return render(request, 'product/product_edit.html', {'form': form})
-
+        return render(request, 'product/product_list.html')
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'product/product_detail.html', {'product': product})
+
+def product_edit(request, pk):
+    current_user = request.user
+    product = get_object_or_404(Product, pk=pk)
+    if current_user == product.seller:
+        if request.method == "POST":
+            form = ProductForm(request.POST, request.FILES, instance=product)
+            if form.is_valid():
+                product = form.save(commit=False)
+                product.seller = current_user
+                product.save()
+                return redirect('product_detail', pk=product.pk)
+        else:
+            form = ProductForm(instance=product)
+        return render(request, 'product/product_edit.html', {'form': form})
+    else:
+        return render(request, 'product/product_list.html')
