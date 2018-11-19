ffa5df806afd927e477079d1c33b7d25e3a70514
hayrettine <hayrettine@anadolu.edu.tr>
2018-11-19 11:15:57 +0300

    fix templates

--- simsar_site/urls.py
+++ simsar_site/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='product/product_list.html'), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
