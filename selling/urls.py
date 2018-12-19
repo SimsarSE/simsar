1f620fa20a7c1f08e6455328bf5426023eb90f87
alperensoydan <alperensoydan@anadolu.edu.tr>
2018-12-19 22:49:07 +0300

    buying process is completed

--- selling/urls.py
+++ selling/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>', views.deal, name='deal'),
+    path('buy/<int:pk>', views.deal, name='buy'),
 ]
--- selling/views.py
+++ selling/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Offers
+from .models import Offers, SoldProduct
 from product.models import Product
 
 def deal(request, pk):
@@ -11,4 +11,14 @@ def deal(request, pk):
     offers = Offers(offer_cost=cost, offer_product=product, offer_seller=seller, offer_bidder=buyer)
     print(offers.offer_cost)
     offers.save()
+    return HttpResponse('')
+
+def buy(request, pk):
+    print("asdasda")
+    buyer = request.user
+    product = Product.objects.get(id=pk)
+    seller = product.seller
+    cost = request.POST['cost']
+    sold = SoldProduct(sold_cost=cost, sold_buyer=buyer, sold_seller=seller, sold_product=product)
+    sold.save()
     return HttpResponse('')
